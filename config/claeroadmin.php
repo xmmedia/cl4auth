5fdf4f313c2d72371f1f6b433c47f22272d52fa2
Darryl Hein <dhein@xmmedia.com>
2010-11-12 22:17:16 +0000

    adding a list of models for cl4admin

---
+++ config/claeroadmin.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	// list of models in this module for cl4admin
+	'model_list' => array(
+		'useradmin' => 'User',
+		'authlog' => 'Auth Log',
+		'authtype' => 'Auth Type',
+		'group' => 'Group',
+		'grouppermission' => 'Group - Permission',
+		'permission' => 'Permission',
+		'usergroup' => 'User - Group',
+	),
+);
