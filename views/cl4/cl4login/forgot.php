66eeebeb5cc1d3c6be8d68a839b0f55b9a6d94be
Darryl Hein <dhein@xmmedia.com>
2012-02-18 15:22:48 -0700

    correcting the forgot password view messages

--- views/cl4/cl4login/forgot.php
+++ views/cl4/cl4login/forgot.php
@@ -4,8 +4,8 @@
 	<p>Please send me a link to reset my password.</p>
 
 <?php echo Form::open(Request::current()); ?>
-	<p>To start, enter email address: <?php echo Form::input('reset_username', '', array('autofocus')); ?></p>
-	<p>Also enter the characters you see in the pictures below.</p>
+	<p>To start, enter your email address: <?php echo Form::input('reset_username', '', array('autofocus')); ?></p>
+	<p>Also enter the characters you see in the CAPTCHA below:</p>
 <?php
 echo recaptcha_get_html(RECAPTCHA_PUBLIC_KEY);
 echo Form::submit(NULL, 'Reset Password');
